0e4874caaf0e7d05b7ab19a6147f8090c1ff6bc0
Cristian Sánchez <devcristiansanchez@gmail.com>
2024-04-25 22:51:03 +0200

    build(docker): add initi sql

---
+++ artifacts/sql/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE microservice_auth WITH
+    OWNER admin
+    TEMPLATE template0
+    ENCODING 'UTF8'
+    TABLESPACE pg_default;
+
+CREATE DATABASE microservice_users WITH
+    OWNER admin
+    TEMPLATE template0
+    ENCODING 'UTF8'
+    TABLESPACE pg_default;
